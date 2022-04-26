f72bd850fe75b0f1f97b24a3c61d301628398ea3
Andreas Finck <mail@heavycross.de>
2022-04-26 10:25:31 +0200

    changed to emacs org mode

--- scripts/archinstall.sh
+++ scripts/archinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-# get CPU Vendor
-#cpuv = lscpu | grep "Vendor ID" | awk '{ print $3}'
+dhclient
+ping -c 5 archlinux.org
+read -p "Last change to abort. If you press enter installation starts and your hdd will be formated."
 
 drive=/dev/sda
 EFI=${drive}1
@@ -9,16 +9,6 @@ part=${drive}2
 mountpoint=/mnt/
 stick=/mnt/usb/
 
-echo
-echo "Creating Network"
-echo
-dhclient
-ping -c 5 archlinux.org
-
-# partition the disk
-echo
-echo "partitioning the drive"
-echo
 parted -a optimal --script $drive -- \
 mklabel gpt  \
    unit mib \
@@ -29,30 +19,16 @@ mklabel gpt  \
    name 2 ROOTFS \
    print
 
-
-read -p "Press enter to continue"
-
-sleep 1
-echo "make filesystems"
-echo
 mkfs.vfat -F 32 -n EFI $EFI
 mkfs.btrfs -f -L "btrfs_pool" ${part}
 
-read -p "Press enter to continue"
-echo
-echo "making btrfs mountpoints"
-echo
 mount ${part} ${mountpoint}
 btrfs sub create ${mountpoint}/@
 btrfs sub create ${mountpoint}/@home
 btrfs sub create ${mountpoint}/@pkg
 btrfs sub create ${mountpoint}/@log
 btrfs sub create ${mountpoint}/@snapshots
-
 umount ${mountpoint}
-
-echo
-echo "mounting btrfs volumes"
 
 mount -o rw,noatime,compress=zstd,space_cache=v2,ssd,discard=async,subvol=@ ${part} ${mountpoint}
 
@@ -67,18 +43,12 @@ mount -o rw,noatime,compress=zstd,space_cache=v2,ssd,discard=async,subvol=@snaps
 mount -o rw,noatime,compress=zstd,space_cache=v2,ssd,discard=async,subvol=@pkg ${part} ${mountpoint}/var/cache/pacman/pkg
 mount -o rw,noatime,compress=zstd,space_cache=v2,ssd,discard=async,subvol=@log ${part} ${mountpoint}/var/log
 
-read -p "Press enter to continue"
-echo
-echo "copying and unpacking stage3"
-echo
-pacstrap ${mountpoint} base linux linux-firmware btrfs-progs vim intel-ucode bash-completion man-pages man-db git sudo 
+pacstrap ${mountpoint} base linux linux-firmware btrfs-progs vim intel-ucode bash-completion man-pages man-db git sudo
 
-echo "Mounting boot partitions\n"
 mount ${EFI} ${mountpoint}/boot/efi
+
 genfstab -U ${mountpoint} >> ${mountpoint}/etc/fstab
 cat ${mountpoint}/etc/fstab
 read -p "Press enter to continue"
 
-echo "change rooting into the system\n"
-arch-chroot ${mountpoint} 
-exit
+arch-chroot ${mountpoint}
--- scripts/wm-config.sh
+++ scripts/wm-config.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
-
-# Variables
 country=Germany
 kbmap=de
-output=Virtual-1
-resolution=1920x1080
-
+#output=Virtual-1
+output=DP-1
+resolution=2560x1440
 # Options
-run_reflector=true
-aur_helper=true
-install_lightdm=false
-gen_xprofile=false
-windowmanager=sway # options are: sway(wayland) i3(xorg) dwm(xorg)
-
+run_reflector=true      # configures the reflector archlinux mirror chooser
+aur_helper=true         # Install YAY or not
+install_lightdm=false   # Install lightdm or not
+gen_xprofile=false      # Generate a xprofile for xorg
+windowmanager=sway      # options are: sway(wayland) i3(xorg) dwm(xorg)
 
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-# function for installing lightdm
 inst_ldm() {
     sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
-    sudo systemctl enable lightdm 
+    sudo systemctl enable lightdm
 }
 
-# create a .xprofile
 set-xprofile() {
     cat > ~/.xprofile << EOF
     setxkbmap $kbmap
@@ -47,8 +42,7 @@ if [[ $get_xprofile = true ]]; then
     set-xprofile
 fi
 
-# Install fonts
-sudo pacman -S --noconfirm dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji python-requests pacman-contrib playerctl pavucontrol archlinux-wallpaper awesome-terminal-fonts ttf-font-awesome 
+sudo pacman -S --noconfirm dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji python-requests pacman-contrib playerctl pavucontrol archlinux-wallpaper awesome-terminal-fonts ttf-font-awesome
 
 get-dwm() {
     # Pull Git repositories and install
@@ -59,7 +53,7 @@ get-dwm() {
         git clone git://git.suckless.org/$repo
         cd $repo;make;sudo make install;cd ..
     done
-    
+
     # XSessions and dwm.desktop
     if [[ ! -d /usr/share/xsessions ]]; then
         sudo mkdir /usr/share/xsessions
@@ -77,13 +71,11 @@ EOF
     sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
 }
 
-
-# Install packages
 swaypkg="sway swaybg waybar foot kitty swayidle swaylock qutebrowser wofi pcmanfm xorg-xwayland polkit-gnome lxappearance playerctl pavucontrol xorg-xhost"
 i3pkg="i3 xorg firefox polkit-gnome nitrogen lxappearance arandr terminator picom dmenu rofi pcmanfm python-requests gnome-system-monitor pacman-contrib playerctl pavucontrol python-dbus dunst"
 
 case $windowmanager in
-sway) 
+sway)
     sudo pacman -S --noconfirm $swaypkg;;
 i3)
     sudo pacman -S --noconfirm $i3pkg;;
@@ -93,6 +85,5 @@ dwm)
     get-dwm ;
    set-xprofile ;;
 esac
-
 
 printf "\e[1;32mDone! you can now reboot.\e[0m\n"
